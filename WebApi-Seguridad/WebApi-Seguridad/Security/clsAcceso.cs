fe87f622e204289538b0e5316df78857dd28b49d
elmarkos23 <elmarkos23@gmail.com>
2018-08-02 17:06:41 -0500

    paso a subir
    pasos iniciación

---
+++ WebApi-Seguridad/WebApi-Seguridad/Security/clsAcceso.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi_Seguridad.Security
+{
+    public class clsAcceso
+    {
+        public static bool VaidateUser(string username, string password)
+        {
+            //autenticacion registrado en codigo, se puede remplazar por otros modos
+            if (username.Equals("admin") && password.Equals("admin"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        } 
+    }
+}
---
+++ WebApi-Seguridad/WebApi-Seguridad/Security/clsAutorizacion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Http.Filters;
+using System.Net.Http;
+using System.Net;
+
+namespace WebApi_Seguridad.Security
+{
+    public class clsAutorizacion : AuthorizationFilterAttribute
+    {
+        public override void OnAuthorization(System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+            if (actionContext.Request.Headers.Authorization == null)
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+            }
+            else
+            {
+                // Gets header parameters  
+                string authenticationString = actionContext.Request.Headers.Authorization.Parameter;
+                string originalString = Encoding.UTF8.GetString(Convert.FromBase64String(authenticationString));
+
+                // Gets username and password  
+                string usrename = originalString.Split(':')[0];
+                string password = originalString.Split(':')[1];
+
+                // Validate username and password  
+                if (!clsAcceso.VaidateUser(usrename, password))
+                {
+                    // returns unauthorized error  
+                    actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+                }
+            }
+
+            base.OnAuthorization(actionContext);
+        } 
+    }
+}
